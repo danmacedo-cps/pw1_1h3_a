d7f7db1c9ed9176510b457c43afb7b0383e1eaf1
Aluno <aluno@etec.sp.gov.br>
2023-11-22 22:38:45 -0300

    AULA2211 ARRAY INTERAÇÕES

--- public/js/action.js
+++ public/js/action.js
@@ -1,10 +1,16 @@
 import userlogin from './moduloDados.js';
 
 const login = document.getElementById("login")
+const cadastro = document.getElementById("cadastro")
 
 login.addEventListener("click", (e) => {
     let user = document.getElementById("usuario").value 
     let senha = document.getElementById("senha").value
-    console.table(user, senha)
-    console.table(userlogin)
 })
+cadastro.addEventListener("click", (e) => {
+    var usuario =userlogin.find((userlogin) => userlogin.email === "victorsena3010@gmail.com")
+    console.log(usuario)
+})
+function encontraUsuario(value, index, array){
+    console.table(value.email)
+}
