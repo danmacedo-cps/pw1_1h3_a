83cf2f66f4ff4d114550f9c8fc8aa9648e1d1856
danilo.macedo@etec.sp.gov.br <aluno@etec.sp.gov.br>
2023-11-28 20:49:42 -0300

    28 11 JS encontrausuario

--- public/js/action.js
+++ public/js/action.js
@@ -6,11 +6,18 @@ const cadastro = document.getElementById("cadastro")
 login.addEventListener("click", (e) => {
     let user = document.getElementById("usuario").value 
     let senha = document.getElementById("senha").value
+    let obj = {email: user, senha: senha}
+    encontraUsuario(obj)
 })
-cadastro.addEventListener("click", (e) => {
-    var usuario =userlogin.find((userlogin) => userlogin.email === "victorsena3010@gmail.com")
-    console.log(usuario)
-})
-function encontraUsuario(value, index, array){
-    console.table(value.email)
+function encontraUsuario(value){
+    if (value.senha === ''){
+        alert("voce não digitou a senha")
+    }else {
+        var usuario = userlogin.find((obj) => obj.email === value.email)
+        if(usuario.senha === value.senha){
+            window.location.href == "127.0.0.1:500/homeUsuario.html"
+        }else{
+            alert("Deu ruim! Para o e-mail: " + value.email)
+        }
+    }
 }
--- public/js/moduloDados.js
+++ public/js/moduloDados.js
@@ -1,6 +1,7 @@
 const userlogin = [
     {id: 1001, username: "victoricoma", senha:"gordinho123", email:"victorsena3010@gmail.com"},
-    {id: 1002, username: "marcelofarofa", senha:"123456", email:"mfarofa@gmail.com"}
+    {id: 1002, username: "marcelofarofa", senha:"123456", email:"mfarofa@gmail.com"},
+    {id: 1003, username: "celsodapadaria", senha: "totoro", email:"celsopadaria@gmail.com"}
 ]
 
 export default userlogin
